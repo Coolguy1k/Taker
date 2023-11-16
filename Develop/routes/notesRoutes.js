d899d5b363323bc06beb39ee6f8cf3911630c770
Chrisly <stlegerchrisly18@gmail.com>
2023-11-15 20:37:40 -0500

    readme update

--- Develop/routes/notesRoutes.js
+++ Develop/routes/notesRoutes.js
@@ -1,54 +1,41 @@
-// express, path, and fs modules; uuidv4 for generating unique id
 const express = require('express');
-const path = require('path');
-const fsPromises = require('fs').promises;
-const { v4: uuidv4 } = require('uuid');
-
-const notesData = require('../db/db.json');
-
-// Create a router
+const { createNewNote, deleteNote } = require('../notes');
 const router = express.Router();
+const notes = require('../db/db.json');
+
+//middleware for parsing incoming JSON data - Edward Kim helped here
 router.use(express.json());
 
-// GET /api/notes
-router.get('/', (req, res) => {
-    res.json(notesData)
-})
+//GET /api/notes - Edward Kim helped here
+router.get('/', (request, response) => {
+  response.json(notes);
+});
 
-// POST /api/notes
-router.post('/', async (req, res) => {
-    req.body.id = uuidv4(); 
-    notesData.push(req.body)
+// Get all notes
+router.get('/notes', (request, response) => {
+  response.json(notes);
+});
 
-    const dbFilePath = path.join(__dirname, '../db/db.json')
-    
-})
-// DELETE /api/notes/:id
-router.delete('/:id', async (req, res) => {
-    // Destructure id from req.params, being sent from the frontend index.js fetch() param
-    const { id } = req.params
-    
-    // Get index of the note with the specific id that matches in the db.json array
-    const noteToDelete = notesData.findIndex(note => note.id === id)
+// Create a new note
+router.post('/notes', async (request, response) => {
+  try {
+    const newNote = { ...request.body, id: notes.length.toString() };
+    const createdNote = createNewNote(newNote, notes);
+    await response.json(createdNote);
+  } catch (error) {
+    await response.status(500).json({ error: 'Sorry, unable to create a note.' });
+  }
+});
 
-    if (noteToDelete === -1) {
-        return res.status(404).json({ error: 'Note with id not found' })
-    }
+// Delete a note by ID
+router.delete('/notes/:id', async (request, response) => {
+  const { id } = request.params;
+  try {
+    notesArr = await deleteNote(id, notes);
+    await response.json(notes);
+  } catch (error) {
+    await response.status(500).json({ error: 'Sorry, unable to delete the note.' });
+  }
+});
 
-    // Remove note from the db.json array
-    notesData.splice(noteToDelete, 1)
-
-    const dbFilePath = path.resolve('db', 'db.json')
-
-    // writeFile() to update db.json array
-    try {
-        await fsPromises.writeFile(dbFilePath, JSON.stringify(notesData, null, 2))
-        res.status(204).json(notesData)
-        console.log(notesData)
-    } catch {
-        console.log("Error deleting note from mock db.json")
-        res.status(500).json({ error: "Error; Note was not written to db.json" })
-    }
-})
-
-module.exports = router
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Note Taker Starter Code
+this is a note taker app where you take notes usning a data base.
+
+notes app works sometimes
